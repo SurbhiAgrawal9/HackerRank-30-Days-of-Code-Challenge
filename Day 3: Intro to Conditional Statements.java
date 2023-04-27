c656d07858eeb695a9e02ebb1fc566288a1b17f5
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-04-27 12:13:18 +0530

    Create Day 3: Intro to Conditional Statements.java

---
+++ Day 3: Intro to Conditional Statements.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+      /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+          if(N%2==0){
+              if(N>=2 && N<=5){
+                  System.out.print("Not Weird");
+              } else if(N>=6 && N<=20){
+                  System.out.print("Weird");
+                  
+              } else if(N>20){
+                  System.out.print("Not Weird");
+                  
+              }
+          } 
+          else {
+                  System.out.println("Weird");
+              
+          }
+
+        bufferedReader.close();
+    }
+}
