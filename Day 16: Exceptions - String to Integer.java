4fd7040d3a28be58678e424a7ad59d502ff6e643
Surbhi Agrawal <111246726+SurbhiAgrawal9@users.noreply.github.com>
2023-07-04 11:38:02 +0530

    Create Day 16: Exceptions - String to Integer.java

---
+++ Day 16: Exceptions - String to Integer.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+          
+
+            Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+           try{
+               int m = Integer.parseInt(s);
+               System.out.println(s);
+           }
+             catch(Exception e){
+                 System.out.println("Bad String");
+             }
+        // String S = bufferedReader.readLine();
+
+        // bufferedReader.close();
+    }
+}
